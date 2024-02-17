59ee58d3dc5386808da76c96be1adb73ed6e4fcc
PreethiMathews <mathewspreethi99@gmail.com>
2024-02-16 22:03:37 -0500

    Create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.scss";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="bg-greynpm-50 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
+        <Link to={"/"}>
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-cyan-600">Home</span>
+            <span className="text-cyan-700">Hub</span>
+          </h1>
+        </Link>
+        <form action="" className="bg-teal-50 p-3 rounded-lg flex item-center w-24 sm:w-64">
+          <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none text-slate-400" />
+          <FaSearch className="text-slate-400" />
+        </form>
+
+        <ul className="flex gap-4">
+          <Link to={"/Home"}>
+            <li className="text-slate-400 hover:scale-110">Home</li>
+          </Link>
+          <Link to={"/About"}>
+            <li className="text-slate-400 hover:scale-110">About</li>
+          </Link>
+
+          <Link to={"/Sign In"}>
+            <li className="text-slate-400 hover:scale-110">Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
