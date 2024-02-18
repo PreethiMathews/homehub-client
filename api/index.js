cbbe100b8a525f03bf0221f5c4c4402b476b6403
PreethiMathews <mathewspreethi99@gmail.com>
2024-02-18 12:53:39 -0500

    Error handling scenarios

--- api/index.js
+++ api/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -15,7 +18,16 @@ mongoose
   });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    statusCode,
+    message,
+  });
+});
 
-app.listen(3002, () => {
+app.listen(3003, () => {
   console.log("Server is running on 3000");
 });
--- api/models/users.model.js
+++ api/models/users.model.js
@@ -3,22 +3,22 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     username: {
-      type: Strign,
+      type: String,
       required: true,
       unique: true,
     },
     email: {
-      type: Strign,
+      type: String,
       required: true,
       unique: true,
     },
     password: {
-      type: Strign,
+      type: String,
       required: true,
     },
   },
   { timestamps: true }
 );
 
-const User = mongoose.model(User, userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
