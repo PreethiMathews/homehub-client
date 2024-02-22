272f9a389ca68f5a28d5483d7f306c40d4182ae0
PreethiMathews <mathewspreethi99@gmail.com>
2024-02-22 06:36:46 -0500

    Update Api Route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 import cors from "cors";
 
@@ -11,6 +12,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO)
