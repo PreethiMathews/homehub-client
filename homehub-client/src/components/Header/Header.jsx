18669cb20f3e11ba08073fef2771ea63227d31d8
PreethiMathews <mathewspreethi99@gmail.com>
2024-02-23 15:23:46 -0500

    header searchbar

--- homehub-client/src/components/Header/Header.jsx
+++ homehub-client/src/components/Header/Header.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import "./Header.scss";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+
 function Header() {
+  const navigate = useNavigate();
+
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  //To persist values in searchbar
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className="bg-gray-100 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
@@ -14,8 +36,8 @@ function Header() {
             <span className="text-cyan-700">Hub</span>
           </h1>
         </Link>
-        <form action="" className="bg-teal-50 p-3 rounded-lg flex item-center w-24 sm:w-64">
-          <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none text-slate-400" />
+        <form onSubmit={handleSubmit} action="" className="bg-teal-50 p-3 rounded-lg flex item-center w-24 sm:w-64">
+          <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none text-slate-400" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
           <FaSearch className="text-slate-400" />
         </form>
 
