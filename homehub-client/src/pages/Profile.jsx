f88608b0cfc98fe47c9c5af11a33dec63a1495c2
PreethiMathews <mathewspreethi99@gmail.com>
2024-02-22 21:25:22 -0500

    Delete listing by listing id

--- homehub-client/src/pages/Profile.jsx
+++ homehub-client/src/pages/Profile.jsx
@@ -70,6 +70,7 @@ function Profile() {
       dispatch(signOutUserFailure(data.message));
     }
   };
+
   const handleShowListings = async () => {
     try {
       setShowListingsError(false);
@@ -85,6 +86,24 @@ function Profile() {
       setShowListingsError(true);
     }
   };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
