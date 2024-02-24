44770f38d7348daca199b01a22f30c73b978de90
PreethiMathews <mathewspreethi99@gmail.com>
2024-02-24 11:50:35 -0500

    Search Query UI impl

--- homehub-client/src/pages/Search.jsx
+++ homehub-client/src/pages/Search.jsx
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Search() {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [listings, setListings] = useState([]);
+
   const [sidebardata, setSidebardata] = useState({
     searchTerm: "",
     type: "all",
@@ -34,16 +38,51 @@ function Search() {
       setSidebardata({ ...sidebardata, sort, order });
     }
   };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+
+    if (searchTermFromUrl) {
+      setSidebardata({
+        searchTerm: searchTermFromUrl || "",
+      });
+    }
+    const fetchListings = async () => {
+      setLoading(true);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/api/listing/get?${searchQuery}`);
+      const data = await res.json();
+      setListings(data);
+      console.log(listings);
+      setLoading(false);
+    };
+
+    fetchListings();
+  }, [location.search]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams();
+    urlParams.set("searchTerm", sidebardata.searchTerm);
+    urlParams.set("type", sidebardata.type);
+    urlParams.set("parking", sidebardata.parking);
+    urlParams.set("furnished", sidebardata.furnished);
+    urlParams.set("offer", sidebardata.offer);
+    urlParams.set("sort", sidebardata.sort);
+    urlParams.set("order", sidebardata.order);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
 
   return (
     <div className="flex flex-col md:flex-row">
       <div className="p-7  border-b-2 md:border-r-2 md:min-h-screen">
-        <form>
+        <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
           <div className="flex items-center gap-2">
             <label className="whitespace-nowrap font-semibold">Search Term:</label>
             <input type="text" id="searchTerm" placeholder="Search..." className="border rounded-lg p-3 w-full" value={sidebardata.searchTerm} onChange={handleChange} />
           </div>
-          <div className="flex gap-2 flex-wrap items-center py-2">
+          <div className="flex gap-2 flex-wrap items-center ">
             <label className="font-semibold">Type:</label>
             <div className="flex gap-2">
               <input type="checkbox" id="all" className="w-5" onChange={handleChange} checked={sidebardata.type === "all"} />
@@ -62,7 +101,7 @@ function Search() {
               <span>Offer</span>
             </div>
           </div>
-          <div className="flex gap-2 flex-wrap items-center py-2">
+          <div className="flex gap-2 flex-wrap items-center ">
             <label className="font-semibold">Amenities:</label>
             <div className="flex gap-2">
               <input type="checkbox" id="parking" className="w-5" onChange={handleChange} checked={sidebardata.parking} />
@@ -73,7 +112,7 @@ function Search() {
               <span>Furnished</span>
             </div>
           </div>
-          <div className="flex items-center gap-2 py-3">
+          <div className="flex items-center gap-2 ">
             <label className="font-semibold">Sort:</label>
             <select onChange={handleChange} defaultValue={"created_at_desc"} id="sort_order" className="border rounded-lg p-3">
               <option value="regularPrice_desc">Price high to low</option>
@@ -82,7 +121,7 @@ function Search() {
               <option value="createdAt_asc">Oldest</option>
             </select>
           </div>
-          <button className="bg-slate-700 text-white p-4 min-w-full rounded-lg uppercase hover:opacity-95 ">Search</button>
+          <button className="bg-slate-700 text-white p-4  rounded-lg uppercase hover:opacity-95 ">Search</button>
         </form>
       </div>
       <div className="flex-1">
